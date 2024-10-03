1f60d6a809cd65a2d063869d2f3226689fc11a16
Rohit Sharma <rohitsharma@gkmit.co>
2024-10-03 14:09:46 +0530

    Add EventPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState<number>(0)
 
   return (
     <>
-      <p className='text-red-600'>{count}</p>
+      <p>{count}</p>
     </>
   )
 }
